438d1d07194f2c09e98c288d275ecfd09cf5c6c7
Danila Sukhanov <codewylle@gmail.com>
2019-01-18 03:47:53 +0500

    Add e2e test

--- packages/meshok-e2e/src/v3.ts
+++ packages/meshok-e2e/src/v3.ts
@@ -1,5 +1,44 @@
 import puppeteer from "puppeteer";
 
+import { matchSnaphotAfterAction, calcSnapshot } from "./v2";
+
 export async function index(page: puppeteer.Page) {
-	console.log("TEST IS UNDER CONSTRUCTION");
+	const button1 = "#root > ul > li > button";
+	const button2 = "#root > ul > li > ul > li > button";
+
+	await matchSnaphotAfterAction(
+		page,
+		async() => {},
+		calcSnapshot(true, true)
+	);
+	await matchSnaphotAfterAction(
+		page,
+		page => page.click(button2),
+		calcSnapshot(true, false)
+	);
+	await matchSnaphotAfterAction(
+		page,
+		page => page.click(button1),
+		calcSnapshot(false)
+	);
+	await matchSnaphotAfterAction(
+		page,
+		page => page.click(button1),
+		calcSnapshot(true, false)
+	);
+	await matchSnaphotAfterAction(
+		page,
+		page => page.click(button2),
+		calcSnapshot(true, true)
+	);
+	await matchSnaphotAfterAction(
+		page,
+		page => page.click(button1),
+		calcSnapshot(false)
+	);
+	await matchSnaphotAfterAction(
+		page,
+		page => page.click(button1),
+		calcSnapshot(true, true)
+	);
 }
